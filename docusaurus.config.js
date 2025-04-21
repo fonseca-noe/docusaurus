92a79ba90c7ce4fb955fe594e36ced177f3ee38f
fonseca-noe <fonseca010101@gmail.com>
2025-04-21 00:29:46 -0500

    feat: gitpages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://fonseca-noe.github,aio/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'fonseca-noe', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
